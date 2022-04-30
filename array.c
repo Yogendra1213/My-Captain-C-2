c13f98139ba2b1c539f65b9162718aa348dea391
Yogendra Singh <90251984+Yogendra1213@users.noreply.github.com>
2022-04-30 15:58:00 +0530

    Create array.c
    PROGRAMME TO PRINT A 3X3 ARRAY AND ALSO PRINT THER DIAGNOL ELEMENT SUM.

---
+++ array.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void main()
+{
+  int arr1[3][3],i,j,sum=0;
+  
+      
+ 
+    /* Stored values into the array*/
+       printf("Input elements in the matrix :\n");
+  for(i=0;i<3;i++)
+  {
+      for(j=0;j<3;j++)
+      {
+	      printf("element - [%d],[%d] : ",i,j);
+	      scanf("%d",&arr1[i][j]);
+      }
+  }  
+ 
+ printf("\nThe matrix is : \n");
+  for(i=0;i<3;i++)
+  {
+      printf("\n");
+      for(j=0;j<3;j++)
+           printf("%d\t",arr1[i][j]);
+  }
+  
+  for (i=0;i<3;i++)
+  {
+      sum=sum+arr1[i][i];
+  }
+  
+  printf("\nThe Sum of the Diagnol Element of the Matrix is:%d",sum);
+ printf("\n\n");
+}
